060e06ebe2d7b1e3da67a84164344c6fea52708f
mohamed Ali <solutiond700@gmail.com>
2023-05-01 00:30:32 +0300

    adding validation schema

--- code/run.py
+++ code/run.py
@@ -1,17 +1,38 @@
 # run.py
-
 from flask import Flask, request
 from flask_restful import Api, Resource
+from marshmallow import Schema, ValidationError, fields
+
+
+class PredictSchema(Schema):
+    age = fields.Integer(missing = 23)
+    gender = fields.String(required=True)
+    seniority = fields.Integer(missing = 11)
+    segment = fields.String(required=True)
+    relationship_type = fields.String(required=True)
+    income = fields.Float(missing = 30000)
+    nationality = fields.String(missing= 'spanish')
+    activity = fields.String(required=True)
+
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class Predict(Resource):
     def post(self):
         data = request.get_json()
-        parameter = data.get('parameter')  
-        
-        return {'hello': 'world', 'parameter': parameter}
+
+        schema = PredictSchema()
+        try:
+            # Validate request body against schema data types
+            result = schema.load(data)
+        except ValidationError as err:
+            # Return a error message if validation fails
+            return {'error': err.messages}, 400
+
+        return {'data': result}
+
 
 api.add_resource(Predict, '/predict')
 
