3529de0c622cb6232942e826992de48354dcf478
mohamed Ali <solutiond700@gmail.com>
2023-04-30 23:51:04 +0300

    post request with parameters

--- code/run.py
+++ code/run.py
@@ -1,12 +1,19 @@
 # run.py
 
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def home():
-    return {'message':'hi'}
+class Predict(Resource):
+    def post(self):
+        data = request.get_json()
+        parameter = data.get('parameter')  
+        
+        return {'hello': 'world', 'parameter': parameter}
+
+api.add_resource(Predict, '/predict')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
